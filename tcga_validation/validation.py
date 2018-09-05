8e17a0e811891efb894ea0292634183af0f9f0b7
José María Fernández <jose.m.fernandez@bsc.es>
2018-09-05 19:56:39 +0200

    Fixed tcga_validation

--- tcga_validation/validation.py
+++ tcga_validation/validation.py
@@ -1,11 +1,12 @@
-from __future__ import division
+from __future__ import division, print_function
 import pandas
+import os
 import sys
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
 parser.add_argument("-i", "--participant_data", help="list of cancer genes prediction", required=True)
-parser.add_argument("-r", "--public_ref", help="list of cancer genes used to validate the predictions", required=True)
+parser.add_argument("-r", "--public_ref_dir", help="directory with the list of cancer genes used to validate the predictions", required=True)
 args = parser.parse_args()
 
 
@@ -13,37 +14,51 @@ def main(args):
 
     # input parameters
     input_participant = args.participant_data
-    public_ref = args.public_ref
+    public_ref_dir = args.public_ref_dir
 
-    validate_input_data(input_participant,  public_ref)
+    validate_input_data(input_participant,  public_ref_dir)
 
 
 
-def validate_input_data(input_participant, public_ref):
+def validate_input_data(input_participant, public_ref_dir):
     # get participant predicted genes
-    participant_data = pandas.read_csv(input_participant, sep='\t',
-                                       comment="#", header=0)
+    try:
+        participant_data = pandas.read_csv(input_participant, sep='\t',
+                                           comment="#", header=0)
+    except:
+        sys.exit("ERROR: Submitted data file {} is not in a valid format!".format(input_participant))
+    
     predicted_genes = list(participant_data.iloc[:, 0].values)
-
-    # get reference dataset to validate against
-    public_ref_data = pandas.read_csv(public_ref, sep='\t',
-                                      comment="#", header=0)
-    cancer_genes = list(public_ref_data.iloc[:, 0].values)
 
     # get ids of the submited fields
     data_fields = ['gene', 'transcript', 'protein_change', 'score', 'pvalue', 'qvalue', 'info']
     submitted_fields = list(participant_data.columns.values)
 
-    ## validate the fields of the submitted data and if the predicted genes are in the mutations file
-    if data_fields == submitted_fields and (set(predicted_genes) < set(cancer_genes)) == True:
-        validated = True
-    else:
-        validated = False
-
+    # get reference dataset to validate against
+    validated = False
+    for public_ref_rel in os.listdir(public_ref_dir):
+        public_ref = os.path.join(public_ref_dir,public_ref_rel)
+        if os.path.isfile(public_ref):
+            try:
+                public_ref_data = pandas.read_csv(public_ref, sep='\t',
+                                                  comment="#", header=0)
+                cancer_genes = list(public_ref_data.iloc[:, 0].values)
+                
+                ## validate the fields of the submitted data and if the predicted genes are in the mutations file
+                if data_fields == submitted_fields and (set(predicted_genes) < set(cancer_genes)) == True:
+                    validated = True
+                else:
+                    print("WARNING: Submitted data does not validate against "+public_ref,file=sys.stderr)
+                    validated = False
+            except:
+                print("PARTIAL ERROR: Unable to properly process "+public_ref,file=sys.stderr)
+                import traceback
+                traceback.print_exc()
+            
     if validated == True:
         sys.exit(0)
     else:
-        sys.exit("ERROR: Submitted data is not in a valid format!")
+        sys.exit("ERROR: Submitted data does not validate against any reference data!")
 
 
 if __name__ == '__main__':
