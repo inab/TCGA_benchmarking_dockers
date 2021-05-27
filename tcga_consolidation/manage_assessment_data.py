03f0e4090eec8722688e5c4f4297b4f2981c7501
José María Fernández <jose.m.fernandez@bsc.es>
2021-05-27 12:28:59 +0200

    Fixed generation of metrics and charts valid both on test data and production

--- tcga_consolidation/manage_assessment_data.py
+++ tcga_consolidation/manage_assessment_data.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 from __future__ import division
-import io
 import shutil
 import json
 import os
@@ -9,6 +8,8 @@ import fnmatch
 from argparse import ArgumentParser
 import numpy as np
 from assessment_chart import assessment_chart
+
+DEFAULT_eventMark = '2018-04-05'
 
 def main(args):
 
@@ -28,7 +29,7 @@ def main(args):
 def read_participant_data(participant_path):
     participant_data = {}
 
-    with io.open(participant_path, mode='r', encoding="utf-8") as f:
+    with open(participant_path, mode='r', encoding="utf-8") as f:
         result = json.load(f)
         for item in result:
             participant_data.setdefault(item['challenge_id'], []).append (item)
@@ -46,50 +47,102 @@ def generate_manifest(data_dir,output_dir, participant_data):
             os.makedirs(cancer_dir)
         participants = []
         
-        cancer_oeb_data = os.path.join(data_dir, cancer+".json")
+        
+        cancer_oeb_data_dir = os.path.join(data_dir, cancer)
+        cancer_oeb_data = cancer_oeb_data_dir + ".json"
 
         if os.path.isfile(cancer_oeb_data):
             # Transferring the public participants data
-            with io.open(cancer_oeb_data, mode='r', encoding="utf-8") as f:
+            with open(cancer_oeb_data, mode='r', encoding="utf-8") as f:
                 aggregation_file = json.load(f)
-                # get id for metrics in x and y axis
-                metric_X = aggregation_file["datalink"]["inline_data"]["visualization"]["x_axis"]
-                metric_Y = aggregation_file["datalink"]["inline_data"]["visualization"]["y_axis"]
-
-                # add new participant data to aggregation file
-                new_participant_data = {}
-                for metrics_data in metrics_file:
-
-                    participant_id = metrics_data["participant_id"]
-                    if metrics_data["metrics"]["metric_id"] == metric_X:
-                        new_participant_data ["metric_x"] = metrics_data["metrics"]["value"]
-                    elif metrics_data["metrics"]["metric_id"] == metric_Y:
-                        new_participant_data ["metric_y"] = metrics_data["metrics"]["value"]
-
-                # copy the assessment files to output directory
-                rel_new_location = participant_id + ".json"
-                new_location = os.path.join(cancer_dir, rel_new_location)
-                with open(new_location, 'w') as f:
-                    json.dump(metrics_file, f, sort_keys=True, indent=4, separators=(',', ': '))
-
-                new_participant_data["participant_id"] = participant_id
-                aggregation_file["datalink"]["inline_data"]["challenge_participants"].append(new_participant_data)
-
-                # add the rest of participants to manifest
-                for name in aggregation_file["datalink"]["inline_data"]["challenge_participants"]:
-                    participants.append(name["participant_id"])
-
-                #copy the updated aggregation file to output directory
-                summary_dir = os.path.join(cancer_dir,cancer + "_summary.json")
-                with open(summary_dir, 'w') as f:
-                    json.dump(aggregation_file, f, sort_keys=True, indent=4, separators=(',', ': '))
-
-                # Let's draw the assessment charts!
-                assessment_chart.print_chart(cancer_dir, summary_dir, cancer, "RAW")
-                assessment_chart.print_chart(cancer_dir, summary_dir, cancer, "SQR")
-                assessment_chart.print_chart(cancer_dir, summary_dir, cancer, "DIAG")
+            
+            # get default id for metrics in x and y axis
+            metric_X = aggregation_file["datalink"]["inline_data"]["visualization"]["x_axis"]
+            metric_Y = aggregation_file["datalink"]["inline_data"]["visualization"]["y_axis"]
         else:
-            print("Skipping {} ({} not found)".format(cancer, cancer_oeb_data))
+            challenge_participants = []
+            
+            # get default id for metrics in x and y axis
+            metric_X = None
+            metric_Y = None
+            for metrics_data in metrics_file:
+                if metric_X is None:
+                    metric_X = metrics_data["metrics"]["metric_id"]
+                elif metric_Y is None:
+                    metric_Y = metrics_data["metrics"]["metric_id"]
+                else:
+                    break
+            
+            # Setting the defaults in case nothing was found
+            if metric_X is None:
+                metric_X = "TPR"
+            if metric_Y is None:
+                metric_Y = "precision"
+            
+            aggregation_file = {
+                "_id": "TCGA:{}_{}_Aggregation".format(DEFAULT_eventMark, cancer),
+                "challenge_ids": [
+                    cancer
+                ],
+                "datalink": {
+                    "inline_data": {
+                        "challenge_participants": challenge_participants,
+                        "visualization": {
+                            "type": "2D-plot",
+                            "x_axis": metric_X,
+                            "y_axis": metric_Y
+                        }
+                    }
+                },
+                "type": "aggregation"
+            }
+            
+            # Get the info from the files in the directory
+            if os.path.isdir(cancer_oeb_data_dir):
+                print("Reading {}".format(cancer_oeb_data_dir))
+                for entry in os.scandir(cancer_oeb_data_dir):
+                    if entry.is_file() and entry.name.endswith(".json"):
+                        with open(entry.path, mode="r", encoding="utf-8") as ep:
+                            metrics_content = json.load(ep)
+                            if metrics_content.get("cancer_type") == cancer:
+                                challenge_participants.append({
+                                    "metric_x": metrics_content["x"],
+                                    "metric_y": metrics_content["y"],
+                                    "participant_id": metrics_content["toolname"]
+                                })
+
+        # add new participant data to aggregation file
+        new_participant_data = {}
+        participant_id = '(unknown)'
+        for metrics_data in metrics_file:
+            participant_id = metrics_data["participant_id"]
+            if metrics_data["metrics"]["metric_id"] == metric_X:
+                new_participant_data["metric_x"] = metrics_data["metrics"]["value"]
+            elif metrics_data["metrics"]["metric_id"] == metric_Y:
+                new_participant_data["metric_y"] = metrics_data["metrics"]["value"]
+
+        # copy the assessment files to output directory
+        rel_new_location = participant_id + ".json"
+        new_location = os.path.join(cancer_dir, rel_new_location)
+        with open(new_location, mode='w', encoding="utf-8") as f:
+            json.dump(metrics_file, f, sort_keys=True, indent=4, separators=(',', ': '))
+
+        new_participant_data["participant_id"] = participant_id
+        aggregation_file["datalink"]["inline_data"]["challenge_participants"].append(new_participant_data)
+
+        # add the rest of participants to manifest
+        for name in aggregation_file["datalink"]["inline_data"]["challenge_participants"]:
+            participants.append(name["participant_id"])
+
+        #copy the updated aggregation file to output directory
+        summary_dir = os.path.join(cancer_dir,cancer + "_summary.json")
+        with open(summary_dir, 'w') as f:
+            json.dump(aggregation_file, f, sort_keys=True, indent=4, separators=(',', ': '))
+
+        # Let's draw the assessment charts!
+        assessment_chart.print_chart(cancer_dir, summary_dir, cancer, "RAW")
+        assessment_chart.print_chart(cancer_dir, summary_dir, cancer, "SQR")
+        assessment_chart.print_chart(cancer_dir, summary_dir, cancer, "DIAG")
 
         #generate manifest
         obj = {
@@ -99,7 +152,7 @@ def generate_manifest(data_dir,output_dir, participant_data):
 
         info.append(obj)
 
-    with io.open(os.path.join(output_dir, "Manifest.json"), mode='w', encoding="utf-8") as f:
+    with open(os.path.join(output_dir, "Manifest.json"), mode='w', encoding="utf-8") as f:
         json.dump(info, f, sort_keys=True, indent=4, separators=(',', ': '))
 
 
